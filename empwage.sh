c5e9001023637bf0727502316013090593c85ded
Anzar-Shaikh <anzarshaikh373@gmail.com>
2021-05-11 12:09:01 +0530

    Emp Present Check

---
+++ empwage.sh
@@ -0,0 +1,19 @@
+
+echo "welcome to employ-wage calculation"
+
+
+emp_present_full_time=0
+emp_present_part_time=1
+emp_absent=2
+
+empCheck=$(( RANDOM%3 ))
+
+if [ $emp_present_full_time -eq $empCheck ]
+then echo "Employ is present full time"
+
+elif [ $emp_present_part_time -eq $empCheck ]
+then echo "Employ is present part time"
+
+else echo "Employ is absent"
+fi
+
