fc739b8a463dc612ee89c4f785c172f4f524a36f
Anzar-Shaikh <anzarshaikh373@gmail.com>
2021-05-11 12:18:34 +0530

    assumptions made  for pay

--- empwage.sh
+++ empwage.sh
@@ -6,6 +6,7 @@ emp_present_full_time=0
 emp_present_part_time=1
 emp_absent=2
 
+
 empCheck=$(( RANDOM%3 ))
 
 if [ $emp_present_full_time -eq $empCheck ]
@@ -16,4 +17,10 @@ then echo "Employ is present part time"
 
 else echo "Employ is absent"
 fi
+
+#assumptions
+
+wage_per_hr=20
+full_day_hr=8
+
 
